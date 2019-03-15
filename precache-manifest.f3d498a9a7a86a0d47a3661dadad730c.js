7bda66a951e22362728509ca27ad71ac0758c2cf
juunone <juunone@gmail.com>
2019-03-15 22:50:41 +0900

    Updates

---
+++ precache-manifest.f3d498a9a7a86a0d47a3661dadad730c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "40d97466779c5abfc5e6",
+    "url": "/react-bingo/static/js/runtime~main.67983e0f.js"
+  },
+  {
+    "revision": "091a2663bf8e903dd8ff",
+    "url": "/react-bingo/static/js/main.584b009a.chunk.js"
+  },
+  {
+    "revision": "b35df97a3178873c0b58",
+    "url": "/react-bingo/static/js/2.fc6e9e4d.chunk.js"
+  },
+  {
+    "revision": "091a2663bf8e903dd8ff",
+    "url": "/react-bingo/static/css/main.87c40679.chunk.css"
+  },
+  {
+    "revision": "bdee63550ed2a727c2586ff5be58f7ed",
+    "url": "/react-bingo/index.html"
+  }
+];
