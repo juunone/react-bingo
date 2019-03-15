c2e9cd140cb2637a9ee7f15aed29b25640977a0c
juunone <juunone@gmail.com>
2019-03-15 23:46:20 +0900

    modified logger

--- src/store.js
+++ src/store.js
@@ -1,10 +1,20 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
-import logger from 'redux-logger'
 
 export default function configureStore(initialState={}) {
+  /* dev redux logger setting */
+  let log = [];
+  if(
+    window.location &&
+    window.location.host &&
+    window.location.host.indexOf('localhost') !== -1){
+      const { logger } = require("redux-logger");
+      log.push(logger);
+  }
+  /* dev redux logger setting */
+
   return createStore(
     rootReducer,
-    applyMiddleware(logger)
+    applyMiddleware(...log)
   );
 }
